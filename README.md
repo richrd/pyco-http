666fa666c72693ee00750903450eb4449f438723
Richard Lewis <richrd.lewis@gmail.com>
2014-11-13 22:02:00 +0200

    Moved description to readme and disabled example server if importing PycoHTTP.

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-pyco-http
-=========
+PycoHTTP
+========
 
 Super minimal "pico sized" python HTTP server (or API interface)
+
+#Only supports:
+* One request at a time, no concurrency
+* Only GET requests
+* Only responds with text/html
+* Does NOT serve files (or anything) by default
+* Responses are defined via a Python callback
+
+#Purpouse:
+* Adding minimal web interfaces to Python apps.
+
+#Why do this?
+* For learning purpouses
+* To use in my projects
--- pyco-http.py
+++ pyco-http.py
@@ -1,24 +1,5 @@
 #!/usr/bin/python
 
-""" PycoHTTP
-
-Super minimal "pico sized" python HTTP server (or API interface)
-
-Only supports:
-- One request at a time, no concurrency
-- Only GET requests
-- Only responds with text/html
-- Does NOT serve files (or anything) by default
-- Responses are defined via a Python callback
-
-Purpouse:
-- Adding minimal web interfaces to Python apps.
-
-Why do this?
-- For learning purpouses
-- To use in my projects
-"""
-
 import sys
 import socket
 import time
@@ -152,6 +133,7 @@ def handle_request(request):
         }
     return response
 
-srv = PycoHTTP()
-srv.set_handler(handle_request)
-srv.listen()
+if __name__ == "__main__":
+    srv = PycoHTTP()
+    srv.set_handler(handle_request)
+    srv.listen()
