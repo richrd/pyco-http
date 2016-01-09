3abb0647f2df1af86d465b94d2955415feb41ec4
Richard Lewis <richrd.lewis@gmail.com>
2016-01-09 13:42:09 +0200

    Added improvements from BX2 ircbot.

--- pyco_http.py
+++ pyco_http.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python
 
+import __future__
+
 import sys
 import time
 import socket
 import select
 import urllib
-import urlparse
+
+if sys.version_info[0] < 3:
+    import urlparse
+    parseurl = urlparse.urlparse
+    unquote = urlparse.unquote
+else:
+    import urllib.parse
+    parseurl = urllib.parse.urlparse
+    unquote = urllib.parse.unquote
 import traceback
 
+
 class Request:
-    def __init__ (self):
-        self.client = (None, None) # IP / Port
+    def __init__(self):
+        self.time = None
+        self.client = (None, None)  # IP / Port
         self.type = ""
         self.uri = ""
+        self.parsed_uri = None
         self.headers = {}
+
 
 class PycoHTTP:
     def __init__(self):
@@ -24,29 +38,57 @@ class PycoHTTP:
         self.socket = None
         self.eol = "\r\n"
         self.max_queued_conns = 5
-        self.max_request_len = 2048 # 2kb max request size
-        self.select_timeout = 0.05
+        self.max_request_len = 2048  # 2kb max request size
+        self.select_timeout = 0.001
+        self.socket_timeout = 0.1
+        self.socket_timeout = 2
         self.headers = {
-            "Content-Type": "text/html",
             "Server": "PycoHTTP",
             "Connection": "close",
+            "Content-Type": "text/html",
+            "Content-Encoding": "utf-8",
         }
         self.request_handler = None
 
     def log(self, s):
         if self.logging:
-            print s
+            print(s)
 
     def error(self, s):
-        print s
+        print(s)
 
-    def get_error_info(self):
-        msg = str(traceback.format_exc()) + "\n" + str(sys.exc_info())
-        return msg
+    def set_port(self, port):
+        self.port = port
+
+    def set_max_queued_conns(self, max_queued_conns):
+        self.max_queued_conns = max_queued_conns
+
+    def set_max_request_len(self, max_request_len):
+        self.max_request_len = max_request_len
+
+    def set_select_timeout(self, select_timeout):
+        self.select_timeout = select_timeout
+
+    def set_socket_timeout(self, socket_timeout):
+        self.socket_timeout = socket_timeout
+
+    def set_default_header(self, header, value):
+        for key in self.headers.keys():
+            if key.lower() == header.lower():
+                self.headers[key] = value
+                return True
+        self.headers[header] = value
+
+    def set_default_headers(self, headers):
+        self.headers = headers
 
     def set_handler(self, request_handler):
         """Set the callback function for handling requests."""
         self.request_handler = request_handler
+
+    def get_error_info(self):
+        msg = str(traceback.format_exc()) + "\n" + str(sys.exc_info())
+        return msg
 
     def start(self, blocking=False):
         """Start the server."""
@@ -54,6 +96,8 @@ class PycoHTTP:
 
         # Avoid "address already in use"
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.settimeout(self.socket_timeout)
+        self.socket.setblocking(False)
 
         self.log("Hosting server...")
         try:
@@ -80,7 +124,10 @@ class PycoHTTP:
         if self.socket in readable:
             try:
                 conn, addr = self.socket.accept()
+                conn.settimeout(self.socket_timeout)
                 self.handle_connection(conn, addr)
+            except KeyboardInterrupt:
+                return False
             except:
                 self.error(self.get_error_info())
 
@@ -90,6 +137,8 @@ class PycoHTTP:
             try:
                 conn, addr = self.socket.accept()
                 self.handle_connection(conn, addr)
+            except KeyboardInterrupt:
+                return False
             except:
                 self.error(self.get_error_info())
 
@@ -98,19 +147,24 @@ class PycoHTTP:
         headers = {}
         for line in lines:
             parts = line.split(":", 1)
-            if len(parts) < 2: # Skip lines without a colon
+            if len(parts) < 2:  # Skip lines without a colon
                 continue
             headers[parts[0].strip().lower()] = parts[1].strip()
         return headers
-
 
     def get_request_data(self, conn):
         """Receive HTTP request data."""
         # Loop and append received data until sufficent data is received
         received = ""
         while received[-4:] != self.eol*2:
+            self.log("Looping receive...")
+            self.log("Data is:'"+str(received)+"'")
             try:
+                self.log("conn.recv...")
                 data = conn.recv(1024)
+                self.log("conn.recv done")
+                if type(data) == bytes:
+                    data = data.decode("utf-8")
             except:
                 return received
             if not data:
@@ -136,31 +190,34 @@ class PycoHTTP:
         lines.pop(0)
 
         request = Request()
+        request.time = time.time()
 
         # Extract request data
         request.type = first[0]
         request.uri = first[1]
+        request.parsed_url = parseurl(request.uri)
         request.client = addr
-
         # Get request heders
         headers = self.parse_headers(lines)
         request.headers = headers
-        
+
         return request
 
     def respond(self, conn, response):
         """Respond to a request."""
 
         # Set response defaults
-        if not "status" in response.keys():
+        if "status" not in response.keys():
             response["status"] = "200"
 
         # Build headers
         header_lines = []
-        headers = self.headers
+        headers = self.headers.copy()
         if "headers" in response.keys():
             # Merge new headers into defaults
-            headers = dict(self.headers.items() + response["headers"].items())            
+            self.log("Headers are:{}".format(response["headers"]))
+            headers.update(response["headers"])
+            self.log("Headers are now:{}".format(headers))
         for header in headers.items():
             header_lines.append(header[0]+": "+header[1])
 
@@ -172,20 +229,50 @@ class PycoHTTP:
 
         # Send the entire response
         # FIXME: may want to check for success and retry when necessary
-        conn.sendall(data)
+        i = 1
+        while i < 11:
+            self.log("Trying to respond with data #{}".format(i))
+            # try:
+            if self.send_all_to_socket(data.encode("utf-8"), conn):
+             # conn.sendall(data.encode("utf-8")) is None:
+                return True
+            # except:
+                # self.log("Attempt #{} failed...".format(i))
+            i += 1
+        self.log("Send failed!")
+        return False
+
+    def send_all_to_socket(self, data, sock):
+        left = data
+        while left != "":
+            # try:
+            data = left
+            # data = left.decode(self.outgoing_encoding)
+            # data = bytes(left, "UTF-8")
+            # data = bytes(left, self.outgoing_encoding)
+            sent = sock.send(data)
+            if len(left) == sent:
+                return True
+            left = left[sent:]
+            # except:
+            #     self.debug_log("send_all_to_socket errored")
+            #     return False
+        return False
+
 
     def handle_connection(self, conn, addr):
         """Handle a HTTP connection."""
         self.log('Connected with ' + addr[0] + ':' + str(addr[1]))
         self.log("Getting request...")
         data = self.get_request_data(conn)
+        self.log("Got request data...")
         request = False
         if data:
             request = self.parse_request(data, addr)
         if request:
             # If we have a request handler give it the request
             if self.request_handler:
-                self.log("Handling reqest...")
+                self.log("Handling request...")
                 response = self.request_handler(request)
                 if response:
                     self.respond(conn, response)
@@ -194,12 +281,12 @@ class PycoHTTP:
             self.log("No request received!")
         conn.close()
 
+
 # Example for a request handler
 def handle_request(request):
     front_uris = ["/", "/index.html"]
-    url = urlparse.urlparse(request["uri"])
-    query = urllib.unquote(url.query)
-
+    # url = parseurl(request.uri)
+    url = request.parsed_url
     if url.path in front_uris:
         response = {
             "data": '<h1>Hello World!</h1>'
@@ -210,10 +297,10 @@ def handle_request(request):
             "data": 'Hello World!\nNew line?'
         }
     elif url.path == "/close":
-        response = False # Don't respond, just close connection
+        response = False  # Don't respond, just close connection
     else:
         response = {
-            "status": 404, # Default status is 200
+            "status": 404,  # Default status is 200
             "data": 'Sorry, not found (404). <a href="/">Front page</a>'
         }
 
@@ -222,8 +309,8 @@ def handle_request(request):
 if __name__ == "__main__":
     srv = PycoHTTP()
     srv.set_handler(handle_request)
-    srv.start(True) # Add true for a blocking server
-    # Otherwise use a loop like this:    
-    # srv.start()
-    # while srv.running:
-    #    srv.serve()
+    #srv.start(True)  # Add true for a blocking server
+    # Otherwise use a loop like this:
+    srv.start()
+    while srv.running:
+       srv.serve()
